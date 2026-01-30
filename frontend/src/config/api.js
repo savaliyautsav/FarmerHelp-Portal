8153f6ddcd01438af170c283c5e7b859100292a8
savaliyautsav <savaliyautsav836@gmail.com>
2026-01-30 22:29:38 +0530

    upadte api

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-});
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
 
-export default api;
+export default API_BASE_URL;
+
